e0dd319ffe153cbac705f99c24d2ce27319918ce
Giorgi Abuladze <g.abuladze@live.com>
2016-04-01 18:47:19 +0400

    fixed icons.

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ function updatePage(forecast) {
 
 function chooseWeatherIcon(forecast) {
   weatherIcon.removeClass();
-  if(20<=(new Date).getHours()<=4) {
+  if((new Date).getHours() <= 4 && (new Date).getHours() >= 20) {
     weatherIcon.addClass("wi wi-owm-night-"+forecast.number);
   } else {
     weatherIcon.addClass("wi wi-owm-day-"+forecast.number);
